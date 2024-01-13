92e9b1642073ace2f88fb59c601c43ffde3e77c9
SlavaMryasov <slavadevbox1@gmail.com>
2024-01-13 17:07:58 +0300

    skills and skill

--- src/components/SectionTitle.tsx
+++ src/components/SectionTitle.tsx
@@ -1,5 +1,5 @@
 import styled from "styled-components";
-
+import { Theme } from "../styles/Theme";
 
 export const SectionTitle = styled.h2`
 text-align: center;
@@ -7,4 +7,21 @@ font-family: "Josefin Sans", "sans-serif";
 font-size: 36px;
 font-weight: 600;
 letter-spacing: 5px;
+position: relative;
+margin-bottom: 90px;
+
+&::before{
+        content:"";
+        display: inline-block;
+        width: 55px;
+        height: 1px;
+        background-color: ${Theme.colors.accent};
+        position: absolute;
+        left: 50%;
+        bottom: -30px;
+        transform: translateX(-50%);
+
+    }
+
+
 `
--- src/layout/sections/skills/skill/Skill.tsx
+++ src/layout/sections/skills/skill/Skill.tsx
@@ -1,32 +1,62 @@
-import styled from "styled-components"
-import { Icon } from "../../../../components/icon/Icon"
+import styled from "styled-components";
+import { Icon } from "../../../../components/icon/Icon";
+import { FlexWrapper } from "../../../../components/FlexWrapper";
 
 type SkillPropsType = {
-    iconId: string,
-    title: string,
-    description: string,
-}
+  iconId: string;
+  title: string;
+  description: string;
+};
 
-export const Skill= (props: SkillPropsType) => {
-    return(
-        <StyledSkill role="menuitem">
-        <Icon iconId={props.iconId}/>
+export const Skill = (props: SkillPropsType) => {
+  return (
+    <StyledSkill role="menuitem">
+      <FlexWrapper direction="column" align="center">
+        <IconWrapper>
+            <Icon iconId={props.iconId} />
+            </IconWrapper>
         <SkillTitle>{props.title}</SkillTitle>
         <SkillText>{props.description}</SkillText>
+      </FlexWrapper>
     </StyledSkill>
-    )
-}
+  );
+};
 
 const StyledSkill = styled.div`
-    width: 30%;
-    background-color: #ffffff64;
-    margin: 10px;
-    border-radius: 7px;
-    padding: 2px;
+  width: 380px;
+  padding: 62px 20px 40px;
+`;
+
+const IconWrapper = styled.div`
+position: relative;
+
+
+&::before {
+    content: "";
+    display: inline-block;
+    width: 80px;
+    height: 80px;
+    background: rgba(255, 255, 255, 0.10);
+    transform: rotate(45deg) translate(-50%, -50%);
+
+    position: absolute;
+    left: 50%;
+    top: 45%;
+    transform-origin: top left;
+    
+}
 `
+
 const SkillTitle = styled.h3`
-    
-`
+  margin: 70px 0 15px;
+  font-family: "Josefin Sans", sans-serif;
+  font-size: 16px;
+  font-weight: 700;
+  letter-spacing: 1px;
+  text-transform: uppercase;
+`;
 const SkillText = styled.p`
-    
-`
+  font-size: 14px;
+  font-weight: 400;
+  line-height: 1.4;
+`;
